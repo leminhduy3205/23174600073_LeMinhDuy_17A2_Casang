433c625636488111f82979b15f330167d699f3aa
Duy Khum Suy <149088242+leminhduy3205@users.noreply.github.com>
2024-03-21 23:42:36 +0700

    bài 2.py

--- bài 2.py
+++ bài 2.py
@@ -1,11 +1,3 @@
-def chu_so_hang_ngan(so_nguyen):
-    if len(str(so_nguyen)) >=4:
-        chu_so_hang_ngan = int(str(so_nguyen)[-4])
-    else:
-        chu_so_hang_ngan = 0
-        return chu_so_hang_ngan
-#---
-so_nguyen=int(input("Nhập một số nguyên: "))
-
-chu_so_hang_ngan = chu_so_hang_ngan(so_nguyen)
-print("Chữ số hàng nghìn của số", so_nguyen, "là:", chu_so_hang_ngan)
+so = int(input("Nhập một số nguyên: "))
+hang_nghin = (so // 1000) % 10
+print("Chữ số hàng nghìn của số đó là:", hang_nghin)
