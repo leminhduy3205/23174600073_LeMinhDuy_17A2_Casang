eebdd7c77ff76485adaf2cd7a2e4452e752df747
Duy Khum Suy <149088242+leminhduy3205@users.noreply.github.com>
2024-03-21 23:48:37 +0700

    bài tập lab 2

--- bài 3.py
+++ bài 3.py
@@ -1,15 +1,63 @@
-# Khởi tạo các biến ban đầu
-initial_amount = 10000  # Số tiền ban đầu
-annual_interest_rate = 0.06  # Lãi suất hàng năm
-
-# Tính toán số tiền sau 5 năm và 10 năm
-amount_after_5_years = initial_amount * (1 + annual_interest_rate)**5
-amount_after_10_years = initial_amount * (1 + annual_interest_rate)**10
-
-# Tính toán tỷ lệ tăng trưởng
-growth_rate = (amount_after_10_years - amount_after_5_years) / amount_after_5_years
-
-# In kết quả ra màn hình
-print(f"Số tiền sau 5 năm: ${amount_after_5_years:.2f}")
-print(f"Số tiền sau 10 năm: ${amount_after_10_years:.2f}")
-print(f"Tỷ lệ tăng trưởng sau 10 năm so với sau 5 năm: {growth_rate:.2%}")
+a = int(input("Nhap vao mot so nguyen bat ki co 3 chu so: "))
+a1 = a//100
+a2 = (a//10)%10
+a3 = a%10
+if a1 == 1:
+    a1 = "one hundred"
+elif a1 == 2:
+    a1 = "two hundred"
+elif a1 == 3:
+    a1 = "three hundred"
+elif a1 == 4:
+    a1 = "four hundred"
+elif a1 == 5:
+    a1 = "five hundred"
+elif a1 == 6:
+    a1 = "six hundred"
+elif a1 == 7:
+    a1 = "seven hundred"
+elif a1 == 8:
+    a1 = "eight hundred"
+elif a1 == 9:
+    a1 = "nine hundred"
+if a2 == 1:
+    a2 = "ten"
+elif a2 == 2:
+    a2 = "twenty"
+elif a2 == 3:
+    a2 = "thirty"
+elif a2 == 4:
+    a2 = "forty"
+elif a2 == 5:
+    a2 = "fifty"
+elif a2 == 6:
+    a2 = "sixty"
+elif a2 == 7:
+    a2 = "seventy"
+elif a2 == 8:
+    a2 = "eighty"
+elif a2 == 9:
+    a2 = "ninety"
+elif a2 == 0 and a3!=0:
+    a2 = ""
+if a3 ==1:
+    a3 ="one"
+if a3 ==2:
+    a3 ="two"
+if a3 ==3:
+    a3 ="three"
+if a3 ==4:
+    a3 ="four"
+if a3 ==5:
+    a3 ="five"
+if a3 ==6:
+    a3 ="six"
+if a3 ==7:
+    a3 ="seven"
+if a3 ==8:
+    a3 ="eight"
+if a3 ==9:
+    a3 ="nine"
+if a3 ==0:
+    a3 =""
+print("Cach doc la:", a1,a2,a3)
