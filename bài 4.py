726b53c4b9d426514c858c6eaed3f22c36a60ddb
Duy Khum Suy <149088242+leminhduy3205@users.noreply.github.com>
2024-03-21 23:44:01 +0700

    bài 4.py

--- bài 4.py
+++ bài 4.py
@@ -1,15 +1,11 @@
-def xep_loai_diem(diem):
-    if 0<= diem<5:
-        return "Điểm kém"
-    elif 5<= diem<=7:
-        return "Điểm trung bình"
-    elif 7<= diem <=8.5:
-        return "Điểm khá"
-    elif 8.5<= diem <= 10:
-        return "Điểm tốt"
-    else:
-        return "Điểm không hợp lệ"
-#---
-diem = float(input("Nhập điểm số của bài kiểm tra: "))
-xep_loai= xep_loai_diem(diem)
-print("Xếp loại điểm:", xep_loai)
+diem_xl = float(input("Yêu cầu nhập điểm: "))
+if 0<= diem_xl<=5:
+    print("Điểm kém")
+elif 5< diem_xl<=7:
+    print("Điểm trung bình")
+elif 7<= diem_xl <=8.5:
+    print ("Điểm khá")
+elif 8.5<= diem_xl <= 10:
+    print ("Điểm tốt")
+else:
+    print ("Điểm không hợp lệ")
