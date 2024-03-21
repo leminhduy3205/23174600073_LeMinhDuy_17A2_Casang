22dfbf89b91cbbf51f0e1c0867f61ea9eb66ef4d
Duy Khum Suy <149088242+leminhduy3205@users.noreply.github.com>
2024-03-21 23:46:19 +0700

    bài 7.py

--- bài 7.py
+++ bài 7.py
@@ -1,15 +1,16 @@
-def xep_loai_chi_so_BMI(chi_so):
-    if chi_so<18.5:
-        return "Gầy"
-    elif 18.5<chi_so<24.9:
-        return "Bình thường"
-    elif 25.0<chi_so<29.9:
-        return "Hơi béo"
-    elif chi_so>=30.0:
-        return "Béo"
-    return "Chỉ số"
-        
-#-----
-chi_so = float(input("nhập chỉ số: "))
-xep_loai = xep_loai_chi_so_BMI(chi_so)
-print("Xếp loại chỉ số", xep_loai)
+chieu_cao = float(input("Nhập chiều cao của bạn (mét): "))
+can_nang = float(input("Nhập cân nặng của bạn (kg): "))
+
+bmi = can_nang / (chieu_cao ** 2)
+print("Chỉ số BMI của bạn là:", bmi)
+
+if bmi < 18.5:
+    print("Phân loại BMI: Gầy")
+elif 18.5 <= bmi <= 24.9:
+    print("Phân loại BMI: Bình thường")
+elif 25.0 <= bmi <= 29.9:
+    print("Phân loại BMI: Hơi béo")
+else:
+    print("Phân loại BMI: Béo")
+
+
